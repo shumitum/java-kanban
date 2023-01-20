8788fcb75814ad6c9fd9fd7ad57cf3ece0c02c54
Shumitum <shumitum@yandex.ru>
2023-01-21 04:12:17 +0500

    ref

--- src/service/Node.java
+++ src/service/Node.java
@@ -4,9 +4,9 @@ import model.Task;
 
 public class Node<T extends Task> { //Класс Node служит единственной цели хранить элементы истории просмотров,
     //а каждый элемент в истории просмотров это Task или наследник класса Task
-    public T data;
-    public Node<T> next;
-    public Node<T> prev;
+    T data;
+    Node<T> next;
+    Node<T> prev;
 
     public Node(Node<T> prev, T data, Node<T> next) {
         this.data = data;
